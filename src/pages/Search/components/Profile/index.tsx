cadf17fb23bbb28043622181659dc2f003b19f95
Demian Tatarcenkas Diniz <demian.dinizdd@campus.mccd.edu>
2021-01-21 22:25:18 -0300

    Add responsive attributes for browser view

--- src/pages/Search/components/Profile/index.tsx
+++ src/pages/Search/components/Profile/index.tsx
@@ -23,15 +23,15 @@ const Profile = ({ profile }: ProfileProps) => (
                 <ButtonApp text="Ver perfil" />
             </a>
         </div>
-        <div className="profile-repository-info">
-            <ul className="repositories">
+        <div className="profile-repository-container">
+            <ul className="repositories-sumary">
                 <li>{`Repositórios públicos: ${profile.public_repos}`} </li>
                 <li>{`Seguidores: ${profile.followers}`} </li>
                 <li>{`Seguindo: ${profile.following}`}  </li>
             </ul>
-            <div className="profile-container-info">
-                <h1 className="profile-title-info">Informações</h1>
-                <input readOnly type="text" className="profile-input" value={`Empresa: ${profile.company}  `}></input>
+            <div className="profile-input-container">
+                <h1 className="profile-title">Informações</h1>
+                <input readOnly type="text" className="profile-input" value={`Empresa: ${profile.company}  `} />
                 <input readOnly type="text" className="profile-input" value={`Website/Blog: ${profile.blog} `} />
                 <input readOnly type="text" className="profile-input" value={`Localidade: ${profile.location} `} />
                 <input readOnly type="text" className="profile-input" value={`Membro desde: ${dayjs(profile.created_at).format('DD/MM/YYYY')} `} />
